bcf693ad729ff95e1b3a0709e42f92d2a6814610
albapc <alba.perez.ce@gmail.com>
2020-02-20 23:00:26 +0100

    agregadas imagenes productos y editado CSS

--- src/app/lista-productos/lista-productos.component.ts
+++ src/app/lista-productos/lista-productos.component.ts
@@ -13,31 +13,73 @@ export class ListaProductosComponent implements OnInit {
     const prod1 = {
       nombre: 'Pan',
       descripcion: 'Pan de masa madre hecho artesanalmente.',
+      img: 'assets/image/bread.png',
       comprado: true
     };
     const prod2 = {
       nombre: 'Pescado',
       descripcion: 'Pescado procedente de mares gallegos. Disponible en diferentes variedades.',
+      img: 'assets/image/fish.png',
       comprado: false
     };
     const prod3 = {
       nombre: 'Carne',
       descripcion: 'Carne de cerdo, vaca...',
+      img: 'assets/image/meat.png',
       comprado: false
     };
     const prod4 = {
       nombre: 'Huevos',
       descripcion: 'Huevos de gallinas camperas.',
+      img: 'assets/image/egg.png',
       comprado: true
     };
     const prod5 = {
-      nombre: 'Pasta',
-      descripcion: 'Pasta de verduras italiana.',
+      nombre: 'Café',
+      descripcion: 'Café procedente de Colombia.',
+      img: 'assets/image/coffee.png',
       comprado: false
     };
     const prod6 = {
       nombre: 'Leche',
       descripcion: 'Leche de vaca, procedente de Galicia.',
+      img: 'assets/image/milk.png',
+      comprado: false
+    };
+    const prod7 = {
+      nombre: 'Queso',
+      descripcion: 'Queso de cabra',
+      img: 'assets/image/cheese.png',
+      comprado: true
+    };
+    const prod8 = {
+      nombre: 'Manzanas',
+      descripcion: 'Manzanas golden',
+      img: 'assets/image/apple.png',
+      comprado: false
+    };
+    const prod9 = {
+      nombre: 'Chocolate',
+      descripcion: 'Chocolate con leche',
+      img: 'assets/image/chocolate.png',
+      comprado: false
+    };
+    const prod10 = {
+      nombre: 'Miel',
+      descripcion: 'Miel de abejas',
+      img: 'assets/image/honey.png',
+      comprado: false
+    };
+    const prod11 = {
+      nombre: 'Cerveza',
+      descripcion: 'Estrella Galicia',
+      img: 'assets/image/beer.png',
+      comprado: false
+    };
+    const prod12 = {
+      nombre: 'Zanahorias',
+      descripcion: 'Dos kilos de zanahorias',
+      img: 'assets/image/carrot.png',
       comprado: false
     };
     this.productos = [];
@@ -47,6 +89,12 @@ export class ListaProductosComponent implements OnInit {
     this.productos.push(prod4);
     this.productos.push(prod5);
     this.productos.push(prod6);
+    this.productos.push(prod7);
+    this.productos.push(prod8);
+    this.productos.push(prod9);
+    this.productos.push(prod10);
+    this.productos.push(prod11);
+    this.productos.push(prod12);
   }
 
 // https://stackoverflow.com/questions/49362462/display-images-and-text-from-ngfor-buttons-angular
