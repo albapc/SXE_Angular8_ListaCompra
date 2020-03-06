27559827f38dba4e84fd5cf21bde1bd5abb561cb
Alba <dam2@perlanegra05.ad.danielcastelao.org>
2020-03-06 13:01:06 +0100

    implementada autenticacion desde Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import {AppComponent} from './app.component';
 import {ListaProductosComponent} from './lista-productos/lista-productos.component';
 import {FormsModule} from '@angular/forms';
 
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {environment} from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,10 @@ import {FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBvAnZzxQohwm4wLBc-Vkr7Ox_0-eGRl6Y',
+    authDomain: 'listacompra-30772.firebaseapp.com',
+    databaseURL: 'https://listacompra-30772.firebaseio.com',
+    projectId: 'listacompra-30772',
+    storageBucket: 'listacompra-30772.appspot.com',
+    messagingSenderId: '1087244156173',
+    appId: '1:1087244156173:web:8a4e6bb5e7afb908484444',
+    measurementId: 'G-HW4RJ1LX7T'
+  }
 };
 
 /*
